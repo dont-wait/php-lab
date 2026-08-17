448729c873e3f71e065309084273d193127d9e54
dont-wait <nguyentansang15042005@gmail.com>
2026-08-17 19:19:12 +0700

    flake

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "PHP lab dev shell for LSP";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { nixpkgs, ... }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forAllSystems = f:
+        nixpkgs.lib.genAttrs systems (system:
+          f (import nixpkgs { inherit system; }));
+    in {
+      devShells = forAllSystems (pkgs:
+        let
+          php = pkgs.php84;
+        in {
+          default = pkgs.mkShell {
+            packages = [
+              php
+              pkgs.php84Packages.composer
+              pkgs.phpactor
+              pkgs.phpstan
+            ];
+          };
+        });
+    };
+}
